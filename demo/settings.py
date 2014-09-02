272cffcc40e9cf124f14b4944aaa34505fe560fd
indu <indu.mary@rapidvaluesolutions.com>
2014-09-02 17:36:30 +0530

    settings updated

--- demo/settings.py
+++ demo/settings.py
@@ -82,10 +82,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT =   os.path.join(BASE_DIR, 'static_media')
+STATIC_ROOT =   os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_media'),
 )
 
 TEMPLATE_DIRS = (
